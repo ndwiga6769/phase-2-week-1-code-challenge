ff5d0f7d69c4e5049c3f9a8c4997e856f4814099
danwycliff <danwycliff6769@gmail.com>
2023-11-17 23:10:47 +0300

    codes

--- src/Search.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Search({myTransaction}){
-    return(
-         <input type="text" placeholder='SEARCH' onChange={myTransaction}/>
-    )
-}
-export default Search;
